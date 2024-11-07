12475d992436d046069d13cfbb31c816c38bd9ab
TrinhLongVu <vucongleo@gmail.com>
2024-11-07 15:17:42 +0700

    okta

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,11 +1,24 @@
 package com.example.demo.config;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
@@ -23,14 +36,47 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(new Filter() {
+                    @Override
+                    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+                        var s = "123";
+                        HttpServletRequest req = (HttpServletRequest) servletRequest;
+                        System.out.println(req.getServletPath());
+                        filterChain.doFilter(servletRequest, servletResponse);
+                    }
+                }, LogoutFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(withDefaults())
+                .oauth2Login(form -> form
+                                .failureHandler(((request, response, exception) -> {
+                                    exception.printStackTrace();
+                                }))
+                        .successHandler(((request, response, authentication) -> {
+                            response.sendRedirect("http://localhost:3000");
+                        }))
+                )
                 .logout(logout -> logout
                         .addLogoutHandler(logoutHandler()));
         return http.build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return request -> {
+            OAuth2User user = delegate.loadUser(request);
+            System.out.println("User Attributes: " + user.getAttributes());
+            return user;
+        };
+    }
+
+    @Bean
+    public HttpFirewall allowSemicolonFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);  // Allow semicolon in URLs
+        return firewall;
     }
 
     private LogoutHandler logoutHandler() {
